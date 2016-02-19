2d70baeade8558da64b6f8010ccc64fa83897347
peng sun <pengsun000@gmail.com>
2016-02-19 13:29:27 -0500

    add NarrowExt

---
+++ LookupTableExt.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: ps
+-- Date: 2/19/16
+-- Time: 1:21 PM
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ NarrowExt.lua
@@ -0,0 +1,26 @@
+--- nn.NarrowExt
+-- extend nn.Narrow in that the updateGradInput can be turned off during bp
+
+local NarrowExt, parent = torch.class('nn.NarrowExt', 'nn.Narrow')
+
+-- class def
+function NarrowExt:__init(...)
+    parent.__init(self, ...)
+    self.flagUpdateGradInput = false
+end
+
+function NarrowExt:updateGradInput(input, gradOutput)
+    if false == self.flagUpdateGradInput then
+        -- simply return the default empty gradInput
+        return self.gradInput
+    else
+        -- use the original method
+        return parent.updateGradInput(self, input, gradOutput)
+    end
+end
+
+-- additional methods
+function NarrowExt:should_updateGradInput(flag)
+    assert(flag==true or flag==false, "flag must be boolean!")
+    self.flagUpdateGradInput = flag
+end
