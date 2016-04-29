e8dca900136ffff3a83de79fe60f6abe2c4964b9
pengsun <pengsun000@gmail.com>
2016-04-29 16:24:29 -0400

    add in-place operation for TemporalAddBias.lua

--- TemporalAddBias.lua
+++ TemporalAddBias.lua
@@ -8,13 +8,15 @@ local function checkInputSize(input)
     return input:size(1), input:size(2), input:size(3)
 end
 
-function TemporalAddBias:__init(inputSize)
+function TemporalAddBias:__init(inputSize, ip)
     parent.__init(self)
 
     assert(type(inputSize) == 'number')
     local C = inputSize
     self.bias = torch.Tensor(1,1,C)
     self.gradBias = torch.Tensor(1,1,C)
+
+    self.ip = ip or true -- default: in place = true
 
     self:reset(0)
 end
@@ -27,17 +29,23 @@ end
 function TemporalAddBias:updateOutput(input)
     local B, M, C = checkInputSize(input)
 
-    self.output:resizeAs(input):copy(input)
+    if true == self.ip then
+        self.output = input
+    else
+        self.output:resizeAs(input):copy(input)
+    end
     self.output:add( self.bias:expand(B,M,C) )
 
     return self.output
 end
 
 function TemporalAddBias:updateGradInput(input, gradOutput)
-    if self.gradInput then
+    if true == self.ip then
+        self.gradInput = gradOutput
+    else
         self.gradInput:resizeAs(gradOutput):copy(gradOutput)
-        return self.gradInput
     end
+    return self.gradInput
 end
 
 function TemporalAddBias:accGradParameters(input, gradOutput, scale)
